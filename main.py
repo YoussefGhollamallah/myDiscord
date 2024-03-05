02cb77371cf8b3948be57163745d356b119c8468
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-05 11:45:56 +0100

    Ajout de fonctionnalité

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from classe import *
 
 
 app = Flask(__name__)
@@ -6,3 +7,25 @@ app = Flask(__name__)
 @app.route("/")
 def bonjour():
     return render_template("index.html")
+
+@app.route("/connexion")
+def connexion():
+    if request.method == "GET":
+        email = request.form["email"]
+        password = request.form["password"]
+        user.connexion(email, password)
+        return render_template("acceuil.html")
+    return render_template("connexion.html")
+
+@app.route("/inscription")
+def inscription():
+    return render_template("inscription.html")
+
+
+@app.route("/traitement")
+def traitement():
+    return "traitement réussi !"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
